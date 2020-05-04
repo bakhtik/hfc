cb82966cb0511c1655a6996063ccca6f3d9d8a0c
Bakhtiyar Rzaliyev <bakhtik@pop-os.localdomain>
2020-05-04 16:06:06 +0500

    chapter 01 added

--- ch01/src/cards.c
+++ ch01/src/cards.c
@@ -8,23 +8,39 @@
 int main()
 {
     char card_name[3];
-    puts("Enter the card name: ");
-    scanf("%2s", card_name);
-    int val = 0;
+    int count = 0;
 
-    switch (card_name[0]) {
-        case 'K': case 'Q': case 'J':
-            val = 10;
-            break;
-        case 'A':
-            val = 11;
-            break;
-        default:
-            val = atoi(card_name);
-            break;
+    while(card_name[0] != 'X') {
+        puts("Enter the card name: ");
+        scanf("%2s", card_name);
+        int val = 0;
+    
+        switch (card_name[0]) {
+            case 'K': case 'Q': case 'J':
+                val = 10;
+                break;
+            case 'A':
+                val = 11;
+                break;
+            default:
+                val = atoi(card_name);
+                if ((val < 1) || (val > 11)) {
+                    puts("I don't understand that value!\n");
+                    continue;
+                }
+                break;
+            case 'X':
+                continue;
+        }
+    
+        /* Check if the value is 3 to 6 */
+        if ((val > 2) && (val < 7))
+            count++;
+        /* Otherwise check if the card was 10, J, Q, or K */
+        else if (val == 10)
+            count--;
+        printf("Current count: %i\n", count);
     }
-
-    printf("The card value is: %i\n", val);
 
     return 0;
 }
